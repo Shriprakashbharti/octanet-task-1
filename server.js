637f97d96c17d9c99f0e9c4c0129a8e0548d2183
Keshav Bharti <bhartikeshav@gmail.com>
2025-05-02 01:15:27 +0530

    url change

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ cloudinary.config({
 
 
 app.use(cors({
-    origin: 'https://shriprakashbharti.github.io',
+    origin: 'https://shriprakashbharti.github.io/portfolio/',
     methods: ['POST']
 }));
 
